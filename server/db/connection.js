bda510949ba044e1d68dd87d42d11e5d5f032031
XianAquino <xian_a@yahoo.com>
2017-04-18 22:33:19 -0700

    graphql user query

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,18 +1,15 @@
-const mongoose = require ('mongoose');
-if (!process.env.dbUrl) {
-  dbUrl = require('../env/config.js').dbUrl
-} else {
-  dbUrl = process.env.dbUrl
-}
-//This must be called because mongoose's promises library is deprecated
+const mongoose = require('mongoose');
+const dbURL = !process.env.dbUrl ? require('../env/config.js').dbUrl : process.env.dbUrl;
+
+// This must be called because mongoose's promises library is deprecated
 mongoose.Promise = global.Promise;
- 
-const mongodbUri = `${dbUrl}`;
+
+const mongodbUri = `${dbURL}`;
 mongoose.connect(mongodbUri);
 
-const conn = mongoose.connection;   
+const conn = mongoose.connection;
 conn.on('error', console.error.bind(console, 'MongoDB connection error:'));
- 
+
 module.exports = conn;
 
 //FYI, if you want play with the data in the command line, use this:
